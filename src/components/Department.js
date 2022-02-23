b3ab8a6dbb2764df8486f80cfe025630f5a1aef6
Alex Cooksey <aecooksey2651@gmail.com>
2022-02-23 18:25:21 -0500

    work on department component

--- src/components/Department.js
+++ src/components/Department.js
@@ -1,8 +1,23 @@
 import React from "react";
 
-function Department () {
+function Department ({ department }) {
+    const { id, name, courses } = department
+    
+    let courseTitles = []
+    courses.map(course => {
+        courseTitles.push(course["title"])
+    })
+    let courseList = courseTitles.join(', ')
     return (
-        console.log('hello')
+        <div className="departmentCard">
+            <h2>{name}</h2>
+            <div>
+                <b>Courses:</b>
+                <br/>
+                {courseList}
+            </div>
+            
+        </div>
     )
 }
 
