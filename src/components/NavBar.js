f81dbba17e77440b6d0f7d87cd7b3bb639133ffd
Alex Cooksey <aecooksey2651@gmail.com>
2022-02-24 14:50:31 -0500

    Begin CSS styling

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,19 +1,46 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 
+
+const linkStyles = {
+  display: "inline-block",
+  width: "150px",
+  padding: "15px",
+  margin: "0 20px 6px",
+  borderRadius: "2%",
+  textDecoration: "none",
+  color: "black",
+};
+
 function NavBar() {
     return (
       <div id="navBar">
-        <NavLink to="/" exact>
+        <NavLink 
+          to="/" exact 
+          style={linkStyles}
+          activeStyle={{background: "antiquewhite",}}
+        >
           Home
         </NavLink>
-        <NavLink to="/students" exact>
+        <NavLink 
+          to="/students" exact
+          style={linkStyles}
+          activeStyle={{background: "antiquewhite",}}
+        >
           Students
         </NavLink>
-        <NavLink to="/courses" exact>
+        <NavLink 
+          to="/courses" exact
+          style={linkStyles}
+          activeStyle={{background: "white",}}
+        >
           Courses
         </NavLink>
-        <NavLink to="/departments" exact>
+        <NavLink 
+          to="/departments" exact
+          style={linkStyles}
+          activeStyle={{background: "antiquewhite",}}
+        >
           Departments
         </NavLink>
       </div>
